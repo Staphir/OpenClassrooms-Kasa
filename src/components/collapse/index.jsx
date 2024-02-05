25e8e982d900c789ee71a0313d06f43243175842
Maxime Dulieu <staphir@gmail.com>
2024-02-05 18:09:32 +0100

    about page

---
+++ src/components/collapse/index.jsx
@@ -0,0 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import "./style.scss";
+import { useState } from "react";
+import { useRef } from "react";
+
+
+function Collapse({title, content}) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [chevronPosition, setChevronPosition] = useState("up")
+    const ref = useRef();
+        
+    const toggleDropdown = () => {
+        if(isOpen) {
+            setIsOpen(false);
+            setChevronPosition("up");
+        } else {
+            setIsOpen(true);
+            setChevronPosition("down");
+        }
+    }
+
+    return (
+        <div className="collapse">
+            <button className="top-bar">
+                {title}
+                <FontAwesomeIcon icon={faChevronUp} className={"chevron " + chevronPosition} onClick={toggleDropdown}></FontAwesomeIcon>
+            </button>
+            <div ref={ref} className="collapse-content" style={isOpen ? {height : ref.current.scrollHeight + "px"} : {height: "0px"}}><p className="collapse-text">{content}</p></div>
+        </div>
+    );
+}
+
+export default Collapse;
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,17 +1,27 @@
 import "../style/about.scss";
 import Banner from "../components/banner";
+import Collapse from "../components/collapse";
 import { Fragment } from "react";
 import aboutBannerImg from "../asset/about_image.png"
 
 
 function About() {
     const altBannerImg = "image de paysage";
+    const collapsesContents = {
+        "Fiabilité": "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements et toutes les informations sont régulièrement vérifiées par nos équipes.",
+        "Respect"  : "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou perturbation du voisinage entraînera une exclusion de notre plateforme.",
+        "Service"  : "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou perturbation du voisinage entraînera une exclusion de notre plateforme.",
+        "Sécurité" : "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+    };
 
     return (
         <Fragment>
             <main>
                 <section className="about-banner">
                     <Banner text="" srcImage={aboutBannerImg} altImage={altBannerImg}></Banner>
+                </section>
+                <section className="about-collapses">
+                    {Object.keys(collapsesContents).map((collapseContent) => <Collapse key={collapseContent} title={collapseContent} content={collapsesContents[collapseContent]}></Collapse>)}
                 </section>
             </main>
         </Fragment>
